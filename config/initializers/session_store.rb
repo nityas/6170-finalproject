75bc9cc3895e7dc6eda213a59e1f358006918773
Ido-Efrati <idoe@mit.edu>
2013-11-14 19:32:12 -0500

    rails new .

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdoEfratiCyjingNityaSubramanianCjcaiFinal::Application.config.session_store :cookie_store, key: '_Ido-Efrati_cyjing_nitya-subramanian_cjcai_final_session'
