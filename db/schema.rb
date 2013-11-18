6ef45d7fb926fbcce699166a68831ad91ed031c2
cyjing <cyjing@ubuntu.(none)>
2013-11-18 16:05:20 -0500

    email working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118154456) do
+ActiveRecord::Schema.define(version: 20131118204544) do
 
   create_table "incomingemails", force: true do |t|
     t.string   "place"
