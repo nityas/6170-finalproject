2e504c0dd0d469f24fa478261c3fa150167ca188
cyjing <cyjing@ubuntu.(none)>
2013-11-18 15:38:20 -0500

    hack hack hack 3

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -11,7 +11,7 @@ class IncomingemailsController < ApplicationController
     #Get the subject line and parse for location
     subjectString = params[:headers]['Subject']
     @location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
-    @sublocation = @location
+    #@sublocation = @location
     #if the regex didn't fing a building location, set location to empty string and call the js function
     if @location.nil?
     	@location = ""
