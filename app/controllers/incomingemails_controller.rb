4b589ba75e3c95784f5ee62e0a38daa6ffef89cb
cyjing <cyjing@ubuntu.(none)>
2013-11-18 16:43:08 -0500

    email working 1.2

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -9,14 +9,14 @@ class IncomingemailsController < ApplicationController
 
   def create
     #Get the subject line and parse for location
-    subjectString = params[:headers]['Subject']
-    @location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
+    #subjectString = params[:headers]['Subject']
+    #@location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
     #@sublocation = @location
     #if the regex didn't fing a building location, set location to empty string and call the js function
-    if @location.nil?
-    	@location = ""
-    end
-    #@location = "13"
+    #if @location.nil?
+    #	@location = ""
+    #end
+    @location = "13"
     puts @location
     respond_to do |format|
       format.html{render :text => 'success', :status => 200}
