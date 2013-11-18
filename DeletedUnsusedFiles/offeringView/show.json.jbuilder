f322962002334590390e0d2de52620c1671404b8
cyjing <cyjing@ubuntu.(none)>
2013-11-17 22:51:29 -0500

    clean up code, deleted unused offering controller methods

---
+++ DeletedUnsusedFiles/offeringView/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @offering, :created_at, :updated_at
--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -3,11 +3,11 @@ class IncomingemailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    # Do some other stuff with the mail message
+    #Get the subject line and parse for location
     subjectString = params[:headers]['Subject']
     location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
-    #if the regex found a building location, call the js querry
-
+    sublocation = location
+    #if the regex didn't fing a building location, set location to empty string and call the js function
     if location.nil?
     	location = ""
     end
@@ -15,7 +15,6 @@ class IncomingemailsController < ApplicationController
     	format.js {}
         format.html { render :text => 'success', :status => 200}
         format.json {}
-        
     end
   end
 end
