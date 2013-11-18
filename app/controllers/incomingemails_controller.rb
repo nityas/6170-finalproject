04c31b186de2d2a275c95a833ae033950f4c1709
cyjing <cyjing@ubuntu.(none)>
2013-11-18 16:07:11 -0500

    email working 1.1

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -13,10 +13,10 @@ class IncomingemailsController < ApplicationController
     @location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
     #@sublocation = @location
     #if the regex didn't fing a building location, set location to empty string and call the js function
-    #if @location.nil?
-    #	@location = ""
-    #end
-    @location = "13"
+    if @location.nil?
+    	@location = ""
+    end
+    #@location = "13"
     puts @location
     respond_to do |format|
       format.html{render :text => 'success', :status => 200}
