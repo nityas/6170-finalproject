ad94d98d00dc936007919ac1d72b41233ae800ac
Carrie Cai <carriecai08@gmail.com>
2013-11-17 22:40:00 -0500

    simplified location create method, but flash still doesnt work

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -49,20 +49,15 @@ class LocationsController < ApplicationController
 
     @location = Location.new(location_params)
 
-    respond_to do |format|
-      if Location.exists?(:customid => location_params[:customid])
-        format.html { render action: 'new' }
+    unless Location.exists?(:customid => location_params[:customid])    
+      if @location.save
+        flash[:notice] = "hurray"
       else
-        if @location.save
-          format.html { redirect_to @location, notice: 'Location was successfully created.' }
-          format.json { render action: 'show', status: :created, location: @location }
-        else
-          format.html { render action: 'new' }
-          format.json { render json: @location.errors, status: :unprocessable_entity }
-        end
-
+        flash[:alert] = "Problem saving this location"
       end
+
     end
+    redirect_to root_path
 
   end
 
