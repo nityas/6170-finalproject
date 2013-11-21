ae0045e17a45894cb9a2822edea4b3241eaf6654
cyjing <cyjing@ubuntu.(none)>
2013-11-21 09:24:43 -0500

    email in heroku

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -9,17 +9,17 @@ class IncomingemailsController < ApplicationController
 
   def create
     #Get the subject line and parse for location
-    subjectString = params[:headers]['Subject']
-    @location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
-    @sublocation = @location
+    #subjectString = params[:headers]['Subject']
+    #@location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
+    #@sublocation = @location
     #if the regex didn't fing a building location, set location to empty string and call the js function
-    if @location.nil?
-    	@location = ""
-    end
+    #if @location.nil?
+    #	@location = ""
+    #end
+    @location = "51"
     respond_to do |format|
+      format.js {render_to_string(:my_location=>location)}
       format.html{render :text => 'success', :status => 200}
-    	format.js {}
-      format.json{}
     end
   end
 end
