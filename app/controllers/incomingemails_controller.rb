b972ea9f8eeff29900ff66df100653a6ecafd9bc
cyjing <cyjing@ubuntu.(none)>
2013-11-21 18:54:15 -0500

    email calling ajax on server side to create offering

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -13,7 +13,7 @@ class IncomingemailsController < ApplicationController
     #Get the subject line and parse for location
     subjectString = params[:headers]['Subject']
     @location = subjectString.match(/(\w?\d{1,2}\w?[-]?\d{0,3})/)
-    @sublocation = @location
+    @sublocation = subjectString
     @description = params[:plain]
     #if the regex didn't fing a building location, set location to empty string and call the js function
     if @location.nil?
@@ -36,8 +36,11 @@ class IncomingemailsController < ApplicationController
     #create the offering
     @offering = Offering.new
     @offering.sub_location = @sublocation
+    puts @sublocation
     @offering.description = @description
+    puts @description
     @offering.location_id = @newLocationId
+    puts @newLocationId
     @offering.save
 
     respond_to do |format|
