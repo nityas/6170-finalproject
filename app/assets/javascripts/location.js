f92d787dce5a6871e09884ba97c2727be7c0eed9
Carrie Cai <carriecai08@gmail.com>
2013-11-30 00:05:46 -0500

    fixed some formatting and fonts in infowindow

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -57,7 +57,8 @@ $(document).ready( function () {
             //open existing location infowindow automatically by finding the marker by the lat/lng
             var epsilon = 0.000001;
             var marker = _.find(markers, function(obj) {
-              return (obj.serviceObject.position.lat() - latitude < epsilon && obj.serviceObject.position.lng() - longitude < epsilon)});
+              return (obj.serviceObject.position.lat() - latitude < epsilon && obj.serviceObject.position.lng() - longitude < epsilon)
+            });
             google.maps.event.trigger(marker.serviceObject, 'click', {latLng: new google.maps.LatLng(0, 0)});
           } else {
             show_location(latitude, longitude, mitlocation_id, result["name"], result["bldgnum"], signed_in);
@@ -70,6 +71,10 @@ $(document).ready( function () {
 
  // Gets html for all locations and updates the view.
   function ajax_show_locations(){
+
+  }
+
+  function ajax_show_location(){
 
   }
 
