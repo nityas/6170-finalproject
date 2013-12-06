af0823dd64fb3e5c119503c299528a2aa4aa5190
Carrie Cai <carriecai08@gmail.com>
2013-12-06 00:19:22 -0500

    changing phonenumber from int to bigint

--- db/migrate/20131130200613_add_phone_to_users.rb
+++ db/migrate/20131130200613_add_phone_to_users.rb
@@ -1,6 +1,6 @@
 class AddPhoneToUsers < ActiveRecord::Migration
   def change
-  	    add_column :users, :phoneNumber, :integer
+  	    add_column :users, :phoneNumber, :bigint
   	    add_column :users, :provider, :string
   end
 end
