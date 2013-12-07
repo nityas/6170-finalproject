f30eb0783795cc1c5333a7bd1e5a07fa3258599e
cyjing <cyjing@ubuntu.(none)>
2013-12-07 11:53:27 -0500

    email redirect 1.2

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -11,30 +11,37 @@ class IncomingemailsController < ApplicationController
     @sublocation = subjectString
     @description = params[:plain]
     #if the location was parse create the location. 
+    successful_email = false
     if !@location.nil?
       response = RestClient.get 'http://whereis.mit.edu/search', {:params => {:type => 'query', :q => @location, :output =>'json'}}
       response = JSON.parse(response)[0]
 
-      #creating a new location or getting the current location
-      @newLocation = Location.new
-      @newLocation.latitude = response["lat_wgs84"]
-      @newLocation.longitude = response["long_wgs84"]
-      @newLocation.title = response["name"]
-      @newLocation.customid = response["id"]
-      @newLocation.building_number = response["bldgnum"]
-      @newLocationId = Location.get_or_create_id(@newLocation)
-
-      #create the offering, should probably do a redirect to preserve rails security
-      #@offering = Offering.new
-      #@offering.sub_location = @sublocation
-      #@offering.description = @description
-      #@offering.location_id = @newLocationId
-      #@offering.save
-
+      if !response.nil?
+        #creating a new location or getting the current location
+        @newLocation = Location.new
+        @newLocation.latitude = response["lat_wgs84"]
+        @newLocation.longitude = response["long_wgs84"]
+        @newLocation.title = response["name"]
+        @newLocation.customid = response["id"]
+        @newLocation.building_number = response["bldgnum"]
+        @newLocationId = Location.get_or_create_id(@newLocation)
+        successful_email = true;
+        #create the offering, should probably do a redirect to preserve rails security
+        #@offering = Offering.new
+        #@offering.sub_location = @sublocation
+        #@offering.description = @description
+        #@offering.location_id = @newLocationId
+        #@offering.save
+      end
     end
     respond_to do |format|
-      format.html{render :text => 'success', :status => 200}
-      format.json{}
+      if successful_email
+        format.html{render :text => 'success', :status => 200}
+        format.json{}
+      else
+        format.html{render :text => 'success', :status => 200}
+        format.json{}
+      end
     end
   end
 end
