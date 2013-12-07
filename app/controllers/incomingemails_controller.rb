0f17e4f03aa86f415c0601659f5e99ef59975fce
cyjing <cyjing@ubuntu.(none)>
2013-12-07 12:45:19 -0500

    get texting from an email

--- app/controllers/incomingemails_controller.rb
+++ app/controllers/incomingemails_controller.rb
@@ -6,11 +6,6 @@ class IncomingemailsController < ApplicationController
   #Treat.install
   skip_before_filter :verify_authenticity_token
 
-  # GET /locations/new
-  def new
-    @incomingemails = Incomingemail.new
-  end
-
   def create
     #Get the subject line and parse for location
     subjectString = params[:headers]['Subject']
@@ -18,29 +13,39 @@ class IncomingemailsController < ApplicationController
     @sublocation = subjectString
     @description = params[:plain]
     #if the location was parse create the location. 
+    successful_email = false
     if !@location.nil?
       response = RestClient.get 'http://whereis.mit.edu/search', {:params => {:type => 'query', :q => @location, :output =>'json'}}
-      response = JSON.parse(response)
+      response = JSON.parse(response)[0]
 
-      #creating a new location or getting the current location
-      @newLocation = Location.new
-      @newLocation.latitude = response[0]["lat_wgs84"]
-      @newLocation.longitude = response[0]["long_wgs84"]
-      @newLocation.title = response[0]["name"]
-      @newLocation.customid = response[0]["id"]
-      @newLocation.building_number = response[0]["bldgnum"]
-      @newLocationId = Location.get_or_create_id(@newLocation)
-
-      #create the offering, should probably do a redirect to preserve rails security
-      @offering = Offering.new
-      @offering.sub_location = @sublocation
-      @offering.description = @description
-      @offering.location_id = @newLocationId
-      @offering.save
+      if !response.nil?
+        #creating a new location or getting the current location
+        @newLocation = Location.new
+        @newLocation.latitude = response["lat_wgs84"]
+        @newLocation.longitude = response["long_wgs84"]
+        @newLocation.title = response["name"]
+        @newLocation.customid = response["id"]
+        @newLocation.building_number = response["bldgnum"]
+        @newLocationId = Location.get_or_create_id(@newLocation)
+        successful_email = true;
+        #create the offering, should probably do a redirect to preserve rails security
+        @offering = Offering.new
+        @offering.sub_location = @sublocation
+        @offering.description = @description
+        @offering.location_id = @newLocationId
+        if @offering.save
+          OffersMailer.offer_mail(@offering)
+        end
+      end
     end
     respond_to do |format|
-      format.html{render :text => 'success', :status => 200}
-      format.json{}
+      if successful_email
+        format.html{redirect_to offerings_path, :from_email => true, :text => 'success', :status => 200}
+        format.json{}
+      else
+        format.html{render :text => 'success', :status => 200}
+        format.json{}
+      end
     end
   end
 end
