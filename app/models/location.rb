2b2ba35cf268d2d4c4fd52ba13cccae857719422
cyjing <cyjing@ubuntu.(none)>
2013-12-08 18:41:53 -0500

    heroku 1.5

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,14 +4,15 @@ class Location < ActiveRecord::Base
 
 
 	#get a locaion id if the location already exists in the location table. Otherwise, creates the location
-	#@param - given a location object
+	#@param - given a location object that may not been already saved
 	#return the location id
 	def self.get_or_create_id(location) 
-		if !Location.exists?(customid: location.customid)
+		if !Location.exists?(customid: location.id)
 			location.save()
 			locationid = location.id
 		else
-			locationid = location.id
+			@mylocation = Location.find_by(customid: location.id)
+			locationid = @mylocation.id
 		end
 		return locationid
 	end
