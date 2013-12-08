c1dcf0acdfc06b1fcd7c642b648aa5e629b6b1c9
Nitya Subramanian <nityas@mit.edu>
2013-12-07 19:24:36 -0500

    controller cleaning

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,6 @@
 class ActivitiesController < ApplicationController
+
+  #public_activity gem-generated model link to get activity feed 
   def index
   	@activities = PublicActivity::Activity.order("created_at desc")
   end
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
 
+  #initialized password reset process by sending an email to the user
   def create
   	user = User.find_by_email(params[:email])
   	user.send_password_reset if User.exists?(user)
@@ -10,10 +11,13 @@ class PasswordResetsController < ApplicationController
   	@user = User.find_by_password_reset_token!(params[:id])
   end
 
+
   def update
   	@user = User.find_by_password_reset_token!(params[:id])
+  	#emailed reset link expires after 2 hours, check if still valid
   	if @user.password_reset_sent_at < 2.hours.ago
   		redirect_to new_password_reset_path, :alert => "Password reset link expired, please re-send"
+  	#updates password_digest in database
   	elsif @user.update_attributes(:password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
         redirect_to root_url, notice: 'User was successfully updated.'
   	else
