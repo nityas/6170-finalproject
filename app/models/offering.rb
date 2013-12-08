20fd582c84aad8e2d175796997f580ad1f35d934
Carrie Cai <carriecai08@gmail.com>
2013-12-08 16:07:11 -0500

    changed some comments

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -22,8 +22,9 @@ class Offering < ActiveRecord::Base
       end
 	end
 
-	# deletes stale offerings (created more than 20 minutes / 1200 seconds ago) from the database. 
-	# This method is called by a cron job every 10 minutes (made short for testing purposes)
+	# Deletes stale offerings (created more than 20 minutes / 1200 seconds ago) from the database. 
+	# This method is called by a cron job every 10 minutes (made short so that the staff can test if they'd like)
+	# To run this just once immediately,  type "heroku run rake remove_stale_offerings" into console
 	def self.remove_stale
 		self.all.each do |offer|
 			if offer.is_stale?(1200)
@@ -32,8 +33,7 @@ class Offering < ActiveRecord::Base
 		end
 	end
 
-	# returns true if this offering was created more than 5 min ago (short time so that staff can test if they desire)
-	# run "heroku run rake remove_stale_offerings" to run this just once immediately
+	# returns true if this offering was created more than grace_period number of seconds ago 
 	def is_stale?(grace_period)
 		seconds_elapsed = Time.now - self.created_at
 		return seconds_elapsed > grace_period
@@ -44,7 +44,9 @@ class Offering < ActiveRecord::Base
 		self.increment!(:numDeleteVotes)
 		session[:votes].push(self.id)
 	end
-	# the numbr of votes that an offering should to be deleted 
+
+	# returns true if this offering has received an adequate number of "votes" 
+	# such that it should be deleted 
 	def sufficient_votes?
 		return self.numDeleteVotes >= 3
 	end
