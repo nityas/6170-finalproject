9ebd0a4986ebec6ce221f2b99100316a743bae0a
Ido-Efrati <idoe@mit.edu>
2013-12-08 22:45:47 -0500

    fixed about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,5 @@ IdoEfratiCyjingNityaSubramanianCjcaiFinal::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.enabled = true
 end
